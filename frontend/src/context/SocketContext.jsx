7d470fe4a1f5ac216ae40d5b3ab5a11c52c2401a
GawliYash20 <yash.gawli76@gmail.com>
2025-02-05 15:39:50 +0530

    latest commit

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("/", { // insert url: "/" if backend serving the static files(frontend)
         query: { userId: authUser._id },
         withCredentials: true,
       });
